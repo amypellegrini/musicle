ac3a2235d4a716655c2dd516b6c32754a8af835d
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-02-05 21:42:34 +0000

    Tidy up styling and layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
-
+	import Row from './components/Row.svelte'
 </script>
 
 <main>
-	<h1>Musicle</h1>	
+	<h1>Musicle</h1>
+	<div class="content-wrapper">
+		<Row />
+	</div>
 </main>
 
 <style>
 	h1 {
 		text-transform: uppercase;
 		text-align: center;
+	}
+
+	.content-wrapper {
+		width: 600px;
+		margin: auto;
 	}
 </style>
