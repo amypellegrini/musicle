97ab5937b9e908d4ce636263869829b6029639d1
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-02-06 14:23:07 +0000

    Remove snapshot (impractical due to svelte dynamic hash in class attribute)

--- src/components/__tests__/Row.spec.js
+++ src/components/__tests__/Row.spec.js
@@ -2,10 +2,6 @@ import Row from "../Row.svelte";
 import { render, screen } from "@testing-library/svelte";
 
 describe("Row", () => {
-  it("renders", () => {
-    expect(render(Row).container).toMatchSnapshot();
-  });
-
   it("displays a time signature", () => {
     render(Row);
     expect(screen.getByTestId("TimeSignature"));
